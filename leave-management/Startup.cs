b9b70511049b2ad6761610f40f8269a15652c394
Alf-Ragnar Wirkola Masternes <alf-ragnar.wirkola@edunord.se>
2020-12-22 17:26:53 +0100

    Scaffoled the items and made the razor pages visible. Minor layout changes and authorization changes. Added seeding to the database to make admin user and admin/employee roles.

--- leave-management/Startup.cs
+++ leave-management/Startup.cs
@@ -37,8 +37,11 @@ namespace leave_management
             services.AddAutoMapper(typeof(Maps));
 
             services.AddDatabaseDeveloperPageExceptionFilter();
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddControllersWithViews();
 
             services.AddRazorPages()
@@ -47,7 +50,12 @@ namespace leave_management
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(
+            IApplicationBuilder app,
+            IWebHostEnvironment env,
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager
+            )
         {
             if (env.IsDevelopment())
             {
@@ -68,6 +76,8 @@ namespace leave_management
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+            SeedData.Seed(userManager, roleManager);
 
             app.UseEndpoints(endpoints =>
             {
